dcdd50bf99a1a2aa10e011f1f0378c5a2bd9eded
Gergő Tisza <tgr.huwiki@gmail.com>
2014-12-05 22:43:21 -0800

    Add sanity tests
    Fullscreen events cannot be triggered without user interaction,
    so more useful tests will have to be written as browser tests,
    but these at least catch typos and other simple errors.
    
    Closes #5.

--- test/test.js
+++ test/test.js
@@ -1,5 +1,12 @@
 QUnit.module( 'jQuery.fullscreen' );
 
-QUnit.test( 'placeholder', 1, function ( assert ) {
-	assert.ok( true, 'placeholder' );
+QUnit.test( 'sanity', 3, function ( assert ) {
+	var $root = $( '<div>' ).appendTo( '#qunit-fixture' );
+
+	assert.ok( $root.enterFullscreen, 'enterFullscreen function is defined' );
+	assert.ok( $root.exitFullscreen, 'exitFullscreen function is defined' );
+	assert.ok( 'fullscreen' in $.support, 'support is detected' );
+
+	// make sure the functions don't throw errors
+	$root.enterFullscreen().exitFullscreen();
 } );
